977cf34a0cd601c8fc0e22c1e3d9f434035f2b19
Satwant-Singh-ADS <89945989+Satwant-Singh-ADS@users.noreply.github.com>
2023-08-09 19:39:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,3 +10,13 @@ for col in numeric_columns:
     plt.title(f'Univariate Histogram of {col}')
     plt.grid(True)
     plt.show()
+
+percentiles = [0.01, 0.02, 0.03, ..., 0.99]  # Specify the percentiles you want to compute
+
+percentiles_data = []
+for col_name in numeric_columns:
+    percentiles_values = Df.approxQuantile(col_name, percentiles, 0.0)
+    percentiles_data.append([col_name] + percentiles_values)
+
+percentile_columns = ["Column"] + [f"P{int(p * 100)}" for p in percentiles]
+percentiles_df = spark.createDataFrame(percentiles_data, percentile_columns)
