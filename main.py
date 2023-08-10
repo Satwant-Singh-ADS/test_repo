0e3051b36fc0544a5987dab37fdd0188ba85574c
Satwant-Singh-ADS <89945989+Satwant-Singh-ADS@users.noreply.github.com>
2023-08-09 20:04:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -20,3 +20,18 @@ for col_name in numeric_columns:
 
 percentile_columns = ["Column"] + [f"P{int(p * 100)}" for p in percentiles]
 percentiles_df = spark.createDataFrame(percentiles_data, percentile_columns)
+
+target_col = "Target"  # Replace with your actual target column name
+target_values = [0, 1]
+
+for col in numeric_columns:
+    plt.figure(figsize=(8, 6))
+    for value in target_values:
+        subset = Df.filter(Df[target_col] == value).select(col, target_col).dropna().toPandas()
+        plt.scatter(subset[col], subset[target_col], label=f"{target_col} = {value}")
+    plt.xlabel(col)
+    plt.ylabel(target_col)
+    plt.title(f'Bivariate Scatter Plot of {col} vs {target_col}')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
